58c3876fa27a8e904810a647a2770218b273cd40
Cal-Ton <67053231+Cal-Ton@users.noreply.github.com>
2025-03-11 16:44:47 -0700

    Add files via upload

--- it161/js/main.js
+++ it161/js/main.js
@@ -1,4 +1,4 @@
-//Nav
+//manages mobile nav 
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -7,11 +7,12 @@ function myFunction() {
         x.className = "topnav";
     }
 }
-//Nav End
 
-//Footer
+//https://tinyurl.com/dynamic-html-checker
+document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+  
+document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href); 
+ 
+//set current year in span with id of this-year
 let d = new Date(); let thisYear = d.getFullYear();
 document.getElementById("this-year").innerHTML = thisYear;
-document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
-document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href); 
- //Footer End
