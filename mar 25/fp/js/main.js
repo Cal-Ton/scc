86774931f7c75cf70eaf02666710074b9bbcc717
Cal-Ton <67053231+Cal-Ton@users.noreply.github.com>
2025-03-29 23:29:50 -0700

    Add files via upload

---
+++ mar 25/fp/js/main.js
@@ -0,0 +1,4 @@
+//Footer
+document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href); 
+ //Footer End
